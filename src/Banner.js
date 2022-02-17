8c1c8535da97af5b494c81f82e41fbee9ab47b59
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-02-18 00:14:31 +0530

    added trailer popup for banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
 import requests from './requests';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 import './Banner.css';
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
   const [showMovieDescription, setShowMovieDescription] = useState(false);
+  const [trailerUrl, setTrailerUrl] = useState('');
 
   useEffect(() => {
     async function fetchData() {
@@ -18,43 +21,76 @@ const Banner = () => {
     fetchData();
   }, []);
 
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    // close trailer window if it is already playing
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.name || '')
+        .then((url) => {
+          const trailerUrl = new URL(url);
+          const v = trailerUrl.searchParams.get('v');
+          setTrailerUrl(v);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
   function truncate(str, max) {
     return str?.length > max ? str.substr(0, max - 1) + '…' : str;
   }
 
   return (
-    <header
-      className="banner"
-      style={{
-        background: 'rgba(0,0,0,0.4)',
-        backgroundSize: 'cover',
-        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
-        backgroundPosition: 'center center',
-        backgroundBlendMode: 'darken',
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner__title">
-          {movie?.title || movie?.name || movie?.original_name}
-        </h1>
+    <>
+      <header
+        className="banner"
+        style={{
+          background: 'rgba(0,0,0,0.4)',
+          backgroundSize: 'cover',
+          backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+          backgroundPosition: 'center center',
+          backgroundBlendMode: 'darken',
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
 
-        <div className="banner__buttons">
-          <button className="banner__button">Play</button>
-          <button className="banner__button">My List</button>
+          <div className="banner__buttons">
+            <button
+              className="banner__button"
+              onClick={() => handleClick(movie)}
+            >
+              Play
+            </button>
+            <button className="banner__button">My List</button>
+          </div>
+
+          <h1
+            className="banner__description"
+            onClick={() => setShowMovieDescription(!showMovieDescription)}
+          >
+            {showMovieDescription
+              ? movie?.overview
+              : truncate(movie?.overview, 150)}
+          </h1>
         </div>
 
-        <h1
-          className="banner__description"
-          onClick={() => setShowMovieDescription(!showMovieDescription)}
-        >
-          {showMovieDescription
-            ? movie?.overview
-            : truncate(movie?.overview, 150)}
-        </h1>
-      </div>
-
-      <div className="banner--fadeBottom"></div>
-    </header>
+        <div className="banner--fadeBottom"></div>
+      </header>
+      {trailerUrl && (
+        <YouTube videoId={trailerUrl} opts={opts} className="banner__trailer" />
+      )}
+    </>
   );
 };
 
