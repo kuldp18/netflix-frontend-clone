5ed9ddb77e7b34fda16ae7b185fb6b992f039f43
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-02-26 13:48:39 +0530

    added code splitting

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
+import React from 'react';
 import './App.css';
-import Row from './Row';
-import Banner from './Banner';
 import requests from './requests';
-import Nav from './Nav';
+const Row = React.lazy(() => import('./Row'));
+const Banner = React.lazy(() => import('./Banner'));
+const Nav = React.lazy(() => import('./Nav'));
+
 function App() {
   return (
     <div className="app">
-      <Nav />
-      <Banner />
-      <Row
-        title="Netflix Originals"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Science Fiction" fetchUrl={requests.fetchSciFiMovies} />
-      <Row title="Crime" fetchUrl={requests.fetchCrimeMovies} />
+      <React.Suspense fallback={<p>Loading...</p>}>
+        <Nav />
+        <Banner />
+
+        <Row
+          title="Netflix Originals"
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow
+        />
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+        <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Science Fiction" fetchUrl={requests.fetchSciFiMovies} />
+        <Row title="Crime" fetchUrl={requests.fetchCrimeMovies} />
+      </React.Suspense>
     </div>
   );
 }
